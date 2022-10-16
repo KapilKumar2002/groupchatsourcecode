a6520c9a5fbe0befc97c0c45305d53045ba97e3e
KapilKumar2002 <kapil012002@gmail.com>
2022-10-17 00:42:42 +0530

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:516422155731:web:9e30a13389624e22980326";
+  static String apiKey = "AIzaSyBuXP8g1hI_6e5tchQ36-GtTA-0k6gQziY";
+  static String messagingSenderId = "516422155731";
+  static String projectId = "chatapp-ed68e";
+  final primaryColor = const Color(0xFFee7b64);
+}
